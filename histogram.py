c2aec510716aca1b1bcee3605adce2f30465a7df
Ed Ng <ed.jk.ng@gmail.com>
2018-12-14 03:37:53 -0800

    add histogram

--- histogram.py
+++ histogram.py
@@ -9,13 +9,28 @@ if __name__ == '__main__':
   files = listdir('images')
 
   for i,col in enumerate(color):
-    histr = np.zeros([256, 1])
+    nearest = np.zeros([256, 1])
+    cnn = np.zeros([256, 1])
+    bilinear = np.zeros([256, 1])
+    high = np.zeros([256, 1])
     for file in files:
-      if 'cnn_ip' in file:
-        img = cv2.imread('images/' + file)
-        histr = np.add(cv2.calcHist([img],[i],None,[256],[0,256]), histr)
+      img = cv2.imread('images/' + file)
+      if 'nearest_ip' in file:
+        nearest = np.add(cv2.calcHist([img],[i],None,[256],[0,256]), nearest)
+      elif 'cnn_ip' in file:
+        cnn = np.add(cv2.calcHist([img],[i],None,[256],[0,256]), cnn)
+      elif 'bilinear_ip' in file:
+        bilinear = np.add(cv2.calcHist([img],[i],None,[256],[0,256]), bilinear)
+      elif 'high_ip' in file:
+        high = np.add(cv2.calcHist([img],[i],None,[256],[0,256]), high)
 
-    plt.plot(histr,color = col)
-    plt.xlim([0,256])
+    plt.subplot(221), plt.plot(np.divide(nearest, len(files)),color = col), plt.xlim([0,256])
+    plt.title('Nearest Neighbor')
+    plt.subplot(222), plt.plot(np.divide(cnn, len(files)),color = col), plt.xlim([0,256])
+    plt.title('CNN')
+    plt.subplot(223), plt.plot(np.divide(bilinear, len(files)),color = col), plt.xlim([0,256])
+    plt.title('Bilinear Interpolation')
+    plt.subplot(224), plt.plot(np.divide(high, len(files)),color = col), plt.xlim([0,256])
+    plt.title('Simulation')
 
   plt.show()
